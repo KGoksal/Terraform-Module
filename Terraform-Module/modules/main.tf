c6c009e639b87d7522d49b4baf37a3b83af495b1
Kadir Goksal <136205319+KGoksal@users.noreply.github.com>
2024-11-18 19:40:41 -0600

    Update main.tf

--- Terraform-Module/modules/main.tf
+++ Terraform-Module/modules/main.tf
@@ -1,11 +1,11 @@
-provider "aws" {
-  region = "us-east-1"
-}
+provider "aws" { 
+  region = "us-east-1" 
+} 
 
-resource "aws_vpc" "module_vpc" {
-  cidr_block = var.vpc_cidr_block
-  tags = {
-    Name = "terraform-vpc-${var.environment}"
+resource "aws_vpc" "module_vpc" { 
+  cidr_block = var.vpc_cidr_block 
+  tags = { 
+    Name = "terraform-vpc-${var.environment}" 
   }
 }
 
